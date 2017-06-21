d8c78d8cb78619c0ef82074e67bd3fecf8aeb603
Sean-Lan <sean.lan.thu@gmail.com>
2017-06-21 11:38:51 +0800

    refactor: move formatTime into Clockface component

--- src/components/clock/Clockface.js
+++ src/components/clock/Clockface.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import {Digit} from './Digit.js';
-import {formatTime} from '../../lib/clockHelpers';
+
+const formatTime = (dateObject) => {
+  let formattedTime = formatLeadingZero(dateObject.getHours()) + ':';
+  formattedTime += formatLeadingZero(dateObject.getMinutes()) + ':';
+  formattedTime += formatLeadingZero(dateObject.getSeconds());
+
+  return formattedTime;
+
+};
+
+const formatLeadingZero = (num) => {
+  return num < 10 ? '0' + num.toString() : num.toString();
+};
 
 export const Clockface = ({dateObject}) => {
 
